923db1da1d329392f4b555b5f61c3e4f75aa19f8
github username <cjab86@gmail.com>
2018-12-12 13:41:02 -0500

    updated server.js path to index.html

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
+app.listen(process.env.PORT || 8080);
