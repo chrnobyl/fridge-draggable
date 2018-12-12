b2fd4ea02310bcdfc7269322926d5ee4a46ab61a
github username <cjab86@gmail.com>
2018-12-12 13:45:54 -0500

    updated server.js path to index.html

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static('client/build'));
+  server.use('*', express.static('client/build')); // Added this
+}
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
