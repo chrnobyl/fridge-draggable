e976c00fcf2c97702957c391badea8c0747c8d41
github username <cjab86@gmail.com>
2018-12-12 13:27:46 -0500

    edit to server.js

--- server.js
+++ server.js
@@ -3,16 +3,6 @@ var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
-
-else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
