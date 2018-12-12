3b7c729c18d6c74e28e3e97d3d8ba026d729fb68
github username <cjab86@gmail.com>
2018-12-12 13:16:29 -0500

    updated server.js for client side routing

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
