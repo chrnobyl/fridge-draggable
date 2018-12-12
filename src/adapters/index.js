2f5ae5e1c0119c007b3c875d1512114194d1ac2d
github username <cjab86@gmail.com>
2018-12-12 11:00:15 -0500

    changed url for database from aws to heroku

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://production.prjkpdvkjk.us-east-1.elasticbeanstalk.com/api/v1/'
+const baseUrl = 'https://react-fridge-api.herokuapp.com/api/v1/'
 
 export default class FridgeAdapter  {
   static all(){
