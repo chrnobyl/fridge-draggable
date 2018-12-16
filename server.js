b55d238c4cfb9db10a3647076a645ce68948e701
github username <cjab86@gmail.com>
2018-12-16 12:04:49 -0500

    moved get into if block

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ app.use(express.static(__dirname + '/'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.use('*', express.static('client/build')); // Added this
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 app.listen(process.env.PORT || 8080);
