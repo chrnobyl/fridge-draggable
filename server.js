69efb419923017f69ff9ab0fa980d377cf2e3304
github username <cjab86@gmail.com>
2018-12-16 11:43:17 -0500

    changed * to / in app.get statement in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('*', express.static('client/build')); // Added this
 }
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(process.env.PORT || 8080);
