6c346f6eee20539367e9a4c34b4c85e18d60f102
github username <cjab86@gmail.com>
2018-12-16 11:46:20 -0500

    reverted last change, switched * to / in app.use

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ app.use(express.static(__dirname + '/'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.use('*', express.static('client/build')); // Added this
+  app.use('/', express.static('client/build')); // Added this
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(process.env.PORT || 8080);
