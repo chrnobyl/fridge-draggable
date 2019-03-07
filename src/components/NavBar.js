a6011f5e462536501183f279152521ad24c0b878
github username <cjab86@gmail.com>
2019-03-07 09:29:34 -0500

    changed nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(props) {
   return (
     <nav className="navbar navbar-inverse">
         <div>
-          <Link to="/" className="navbar-brand">Homeeee</Link>
+          <Link to="/" className="navbar-brand">Home</Link>
           <Link to="/fridge" className="navbar-brand">The Fridge</Link>
           <Link to="/freezer" className="navbar-brand">The Freezer</Link>
           <Link to="/drawers" className="navbar-brand">My Drawers</Link>
